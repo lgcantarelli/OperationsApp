130ecc5497aabc0feabc0ce2765d8e3747253448
matheusjkweber <matheus.jk.weber@gmail.com>
2016-05-07 00:54:29 -0300

    FiltredList added.

---
+++ OperationsApp.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operationsapp;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class OperationsApp {
+    public static List<ChargeCategory> chargeCategories;
+    public static List<RevenueCategory> revenueCategories;
+    
+    private static void initialize_categories(){
+        chargeCategories = new ArrayList<ChargeCategory>();
+        revenueCategories = new ArrayList<RevenueCategory>();
+        
+        chargeCategories.add(new ChargeCategory(1,"Supermecado"));
+        chargeCategories.add(new ChargeCategory(2,"Aluguel"));
+        chargeCategories.add(new ChargeCategory(3,"Luz"));
+        chargeCategories.add(new ChargeCategory(4,"Água"));
+        chargeCategories.add(new ChargeCategory(5,"Telefone"));
+        chargeCategories.add(new ChargeCategory(6,"Internet"));
+        chargeCategories.add(new ChargeCategory(7,"Celular"));
+        
+        revenueCategories.add(new RevenueCategory(8,"Salário"));
+        revenueCategories.add(new RevenueCategory(9,"Bolsa"));
+        revenueCategories.add(new RevenueCategory(10,"Freelance"));
+        revenueCategories.add(new RevenueCategory(11,"Outros"));
+    }
+    
+    private ChargeCategory getChargeCategory(int id){
+        for(int i = 0; i < chargeCategories.size(); i++){
+            if(chargeCategories.get(i).getId() == id){
+                return chargeCategories.get(i);
+            }
+        }
+        return null;
+    }
+    
+    private ChargeCategory getChargeCategory(String name){
+        for(int i = 0; i < chargeCategories.size(); i++){
+            if(chargeCategories.get(i).getName().equals(name)){
+                return chargeCategories.get(i);
+            }
+        }
+        return null;
+    }
+    
+    private RevenueCategory getRevenueCategory(int id){
+        for(int i = 0; i < revenueCategories.size(); i++){
+            if(revenueCategories.get(i).getId() == id){
+                return revenueCategories.get(i);
+            }
+        }
+        return null;
+    }
+    
+    private RevenueCategory getRevenueCategory(String name){
+        for(int i = 0; i < revenueCategories.size(); i++){
+            if(revenueCategories.get(i).getName().equals(name)){
+                return revenueCategories.get(i);
+            }
+        }
+        return null;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ParseException {
+        initialize_categories();
+        
+        // TODO code application logic here
+        User user = new User();
+        
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = formatter.parse("07/05/2016");
+        Date date1 = formatter.parse("12/05/2016");
+        Date date2 = formatter.parse("07/04/2016");
+        Date date3 = formatter.parse("12/04/2016");
+        Date date4 = formatter.parse("07/03/2016");
+        Date date5 = formatter.parse("12/03/2016");
+        Date date6 = formatter.parse("07/02/2016");
+        Date date7 = formatter.parse("01/02/2016");        
+        
+        user.add_revenue(new Revenue(user.get_last_id(), 3, "Teste R", date, revenueCategories.get(1)));
+        user.add_revenue(new Revenue(user.get_last_id(), 4, "Teste R 1", date1, revenueCategories.get(2)));
+        user.add_revenue(new Revenue(user.get_last_id(), 2, "Teste R 2 ", date2, revenueCategories.get(3)));
+        user.add_revenue(new Revenue(user.get_last_id(), 5, "Teste R 3", date3, revenueCategories.get(1)));
+        
+        user.add_charge(new Charge(user.get_last_id(), 3, "Teste C", date4, chargeCategories.get(1)));
+        user.add_charge(new Charge(user.get_last_id(), 4, "Teste C 1", date5, chargeCategories.get(2)));
+        user.add_charge(new Charge(user.get_last_id(), 2, "Teste C 2 ", date7, chargeCategories.get(3)));
+        user.add_charge(new Charge(user.get_last_id(), 5, "Teste C 3", date, chargeCategories.get(1)));
+        
+        
+        
+        //user.del_operation(2);
+        
+        user.print_operations();
+        
+        double a[][] = user.return_line_date();
+        for(int i = 0; i < 12; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.println("Mes: "+i+" Valor: "+j+" "+a[i][j]);
+            }
+        }
+        
+ 
+        List<Operation> list = user.getFiltredList("05/02/2016","01/04/2016",true,true);
+        
+        for(int i = 0; i < list.size(); i++){
+            list.get(i).toString();
+        }
+        
+        //System.out.println(user.get_balance());
+        
+        //user.del_operation_by_id(4);
+       //user.print_operations();
+        //System.out.println(user.get_balance());
+        
+        //user.edit_operation(3, 10, "Teste R Editada", new Date(new Date().getTime() + (1000 * 60 * 60 * 24)), revenueCategories.get(3), null);
+        //user.print_operations();
+        //System.out.println(user.get_balance());
+        
+        //user.edit_operation(7, 10, "Teste C Editada", new Date(new Date().getTime() + (1000 * 60 * 60 * 24)), null, chargeCategories.get(1));
+       // user.print_operations();
+       // System.out.println(user.get_balance());
+    }
+    
+}
---
+++ User.java
@@ -0,0 +1,396 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operationsapp;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+public class User {
+    private List<Operation> operations;
+    private int last_id;
+    private double balance;
+    
+    private static final String COMMA_DELIMITER = ",";
+    private static final String NEW_LINE_SEPARATOR = "\n";
+    private static final String FILE_HEADER = "id,type,value,category,description";
+    private static final String fileName = System.getProperty("user.home")+"/operations.csv";
+    
+    /**
+     * Initialize the User with a empty list of operations and call the method to populate it.
+     */
+    public User(){
+        operations = new ArrayList<Operation>();
+        populate_initial();
+    }
+    
+    /**
+     * @return the operations
+     */
+    public List<Operation> getOperations() {
+        return operations;
+    }
+
+    /**
+     * @param operations the operations to set
+     */
+    public void setOperations(List<Operation> operations) {
+        this.operations = operations;
+    }
+    
+    /**
+     * Open the .csv file and populate the list of operations in the date order.
+     */
+    private void populate_initial(){
+        
+    }
+    
+    /**
+     * Add a revenue  in the list of operations in the date order, update the last id.
+     * @param r, a Revenue object.
+     */
+    public void add_revenue(Revenue r){
+        int i = 0;
+                
+        if(operations.size() == 0){
+           operations.add(r);
+        }else{
+            for(; i < operations.size(); i++){
+                if(r.getDatetime().before(operations.get(i).getDatetime())){
+                    break;
+                }
+            }
+            if(i == operations.size()){
+                operations.add(r);
+            }else{
+                Operation aux;
+                Operation aux1;
+                aux = operations.get(i);
+                operations.set(i, r);
+                i++;
+
+                for(; i < operations.size(); i++){
+                   aux1 = operations.get(i);
+                   operations.set(i,aux);
+                   aux = aux1;
+                }
+
+                operations.add(aux);
+            }
+           
+        }
+        balance = balance + r.getValue();
+        last_id++;
+        //rewrite_csv();
+    }
+    
+    /**
+     * Add a revenue  in the list of operations in the date order, update the last id.
+     * @param value, the value of the revenue.
+     * @param datetime, the date of the revenue.
+     */
+    public void add_revenue(double value,String description, Date datetime,RevenueCategory category){
+        Revenue revenue=new Revenue(last_id,value,description,datetime,category);
+        operations.add(revenue);
+    }
+    
+    /**
+     * Add a charge  in the list of operations in the date order, update the last id.
+     * @param r, a Charge object.
+     */
+    public void add_charge(Charge c){
+        int i = 0;
+                
+        if(operations.size() == 0){
+           operations.add(c);
+        }else{
+            for(; i < operations.size(); i++){
+                if(c.getDatetime().before(operations.get(i).getDatetime())){
+                    System.out.println(i);
+                    break;
+                }
+            }
+            if(i == operations.size()){
+                operations.add(c);
+            }else{
+                Operation aux;
+                Operation aux1;
+                aux = operations.get(i);
+                operations.set(i, c);
+                i++;
+
+                for(; i < operations.size(); i++){
+                   aux1 = operations.get(i);
+                   operations.set(i,aux);
+                   aux = aux1;
+                }
+
+                operations.add(aux);
+            }
+           
+        }
+        balance = balance - c.getValue();
+        last_id++;
+    }
+    
+    /**
+     * Add a charge  in the list of operations in the date order, update the last id.
+     * @param value, the value of the charge.
+     * @param datetime, the date of the charge.
+     */
+    public void add_charge(double value, String description, Date datetime,ChargeCategory category){
+        Charge charge=new Charge(last_id,value,description,datetime,category);
+        operations.add(charge);
+    }
+    
+    /**
+     * Return the csv file to the user.
+     * @return file, a file to be handled by the GUI.
+     */
+    public File export_file(){
+        return new File("");
+    }
+
+    /**
+     * Returns a Operation by position.
+     * @param position, the position in the array.
+     * @return Operation object.
+     */
+    public Operation get_operation(int position){
+        return operations.get(position);
+    }
+    
+    /**
+     * Returns a Operation by id.
+     * @param position, the id of the operation.
+     * @return Operation object.
+     */
+    public Operation get_operation_by_id(int id){
+        for(int i=0;i<operations.size();i++){
+           if (id==operations.get(i).getId()){
+               return operations.get(i);
+                
+            }
+            
+        }
+        return null;
+    }
+    
+    /**
+     * Edit a operation by id.
+     * @param id, the id to search.
+     * @param value, the new value of the operation.
+     * @param datetime, the new date of the operation.
+     */
+     public void edit_operation(int id, double value, String description, Date datetime, RevenueCategory category, ChargeCategory category1){
+        for(int i=0;i<operations.size();i++){
+            if(id==operations.get(i).getId()){
+                if(operations.get(i) instanceof Revenue){
+                    Revenue r = (Revenue) operations.get(i);
+                    r.setCategory(category);
+                    balance = balance - operations.get(i).getValue() + value;
+                }else{
+                    Charge c = (Charge) operations.get(i);
+                    c.setCategory(category1);
+                    balance = balance + operations.get(i).getValue() - value;
+                }
+                operations.get(i).edit_operation(value, description, datetime);
+                
+                rewrite_csv();
+                
+            }
+            
+        }
+    }
+    
+    
+    /**
+     * Delete a Operation by position.
+     * @param position, the position in the array.
+     */
+    public void del_operation(int position){
+        if(operations.get(position) instanceof Revenue){
+            Revenue r = (Revenue) operations.get(position);
+            balance = balance - operations.get(position).getValue();
+        }else{
+            Charge c = (Charge) operations.get(position);
+            balance = balance + operations.get(position).getValue();
+        }
+        operations.remove(position);
+        rewrite_csv();
+    }
+    
+    /**
+     * Delete a Operation by id.
+     * @param id, the id of the operation.
+     */
+    public void del_operation_by_id(int id){
+        for(int i=0;i<operations.size();i++){
+            if(id==operations.get(i).getId()){
+                del_operation(i);
+                rewrite_csv();
+            }
+        }
+    }
+    
+    /**
+     * Return the data to populate the report.
+     */
+    public void return_bar_data(){
+        
+    }
+    
+    /**
+     * Return the data to populate the report.
+     */
+    public void return_pizza_data(){
+        
+    }
+    
+    public double[][] return_line_date(){
+        double data[][] = new double[12][3];
+        double revenue = 0;
+        double charge = 0;
+        int month = 1;
+        Calendar cal1 = Calendar.getInstance();
+        for(int i = 0; i < operations.size();i++){
+            cal1.setTime(operations.get(i).getDatetime());
+            
+            if(operations.get(i).getType() == 1){
+                data[cal1.get(Calendar.MONTH)][0] = data[cal1.get(Calendar.MONTH)][0] + operations.get(i).getValue();
+                data[cal1.get(Calendar.MONTH)][2] = data[cal1.get(Calendar.MONTH)][2] + operations.get(i).getValue();
+            }else{
+                data[cal1.get(Calendar.MONTH)][1] = data[cal1.get(Calendar.MONTH)][1] + operations.get(i).getValue(); 
+                data[cal1.get(Calendar.MONTH)][2] = data[cal1.get(Calendar.MONTH)][2] - operations.get(i).getValue();
+            }
+        }
+        
+        return data;
+    }
+    
+    /**
+     * Return the last_id.
+     * @return last_id.
+     */
+    public int get_last_id(){
+        return last_id;
+    }
+    
+    /**
+     * Return the actual balance.
+     * @return balance.
+     */
+    public double get_balance(){
+        return balance;
+    }
+    
+    private void rewrite_csv(){
+        FileWriter fileWriter = null;
+        
+        try {
+            fileWriter = new FileWriter(fileName);
+
+            fileWriter.append(FILE_HEADER.toString());
+
+            fileWriter.append(NEW_LINE_SEPARATOR);
+
+            for (Operation operation : operations) {
+                String category = "";
+                String type = "";
+                if(operation.getType() == 1){
+                    Revenue op = (Revenue) operation;
+                    category = op.getCategory().getName();
+                    type = "Revenue";
+                }else{
+                    Charge op = (Charge) operation;
+                    category = op.getCategory().getName();
+                    type = "Charge";
+                }
+                fileWriter.append(String.valueOf(operation.getId()));
+
+                fileWriter.append(COMMA_DELIMITER);
+
+                fileWriter.append(type);
+
+                fileWriter.append(COMMA_DELIMITER);
+
+                fileWriter.append(String.valueOf(operation.getValue()));
+
+                fileWriter.append(COMMA_DELIMITER);
+
+                fileWriter.append(category);
+
+                fileWriter.append(COMMA_DELIMITER);
+
+                fileWriter.append(String.valueOf(operation.getTitle()));
+
+                fileWriter.append(NEW_LINE_SEPARATOR);
+
+            }
+
+               System.out.println("CSV file was created successfully !!!");
+        } catch (Exception e) {
+            System.out.println("Error in CsvFileWriter !!!");
+            e.printStackTrace();
+        } finally {
+            try {
+
+                fileWriter.flush();
+
+                fileWriter.close();
+
+            } catch (IOException e) {
+
+                System.out.println("Error while flushing/closing fileWriter !!!");
+
+                e.printStackTrace();
+
+            }
+        }
+
+    }
+    
+    public void print_operations(){
+        for(int i = 0; i < operations.size(); i++){
+            operations.get(i).toString();
+        }
+    }
+    
+    public List<Operation> getFiltredList(String from, String to, boolean revenue, boolean charge) throws ParseException{
+        List<Operation> list = new ArrayList<Operation>();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = formatter.parse("01/01/1900");
+        Date date1 = formatter.parse("01/01/2999");
+        if(from.length()>0){
+            date  = formatter.parse(from);
+        }
+        if(to.length()>0){
+            date1 = formatter.parse(to);
+        }
+        
+        for(int i = 0; i < operations.size();i++){
+            if(operations.get(i).getDatetime().after(date) && operations.get(i).getDatetime().before(date1)){
+                if(operations.get(i).getType() == 1 && revenue == true){
+                   list.add(operations.get(i));
+                }
+                
+                if(operations.get(i).getType() == 2 && charge == true){
+                   list.add(operations.get(i));
+                }
+            }
+            
+        }
+        return list;
+        
+    }
+}
